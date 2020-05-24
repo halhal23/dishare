d4290ffb1fa667f30aa69b558763df5f07cf89ef
Hiroharu Hara <hh.25799623@gmail.com>
2020-05-24 12:30:25 +0900

    [design] ぐるなびAPIによる検索結果一覧のレイアウトを決定。詳細なデザインは未実装。

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -23,7 +23,8 @@ export default {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '~/assets/styles/common.css'
   ],
   /*
   ** Plugins to load before mounting the App
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="container">
+  <div>
+    <div class="main-container">
       <img src="images/dishare-main-logo.png" width="400px" height="300px">
-      <h2>Dishareへようこそ</h2>
-      <h3>「美味しい」を共有しましょう</h3>
+      <h2>
+        Dishareへようこそ <br>
+        「美味しい」を共有しましょう
+      </h2>
       <p>早速、キーワードを検索して、「美味しい」を検索してみましょう</p>
-      <el-input placeholder="Please input" v-model="keyword" class="input-with-select" style="width:500px;">
+
+      <el-input placeholder="Please input" v-model="keyword" class="input-with-select" style="width:600px;">
+        <el-select v-model="select" slot="prepend" placeholder="現在位置から探す" style="width:200px;">
+          <el-option label="現在位置から探す" value="1"></el-option>
+          <el-option label="人気店を探す" value="2"></el-option>
+        </el-select>
         <el-button @click="getShops" slot="append" icon="el-icon-search"></el-button>
       </el-input>
-     
+    </div>
+    <ul class="searched-list">
+      <li class="shop-card" v-for="shop in shops" :key="shop.name">
+        {{ shop.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -15,7 +28,9 @@
 export default {
   data(){
     return {
-      keyword: ''
+      keyword: '',
+      select: '',
+      shops: []
     }
   },
   methods: {
@@ -23,12 +38,19 @@ export default {
       this.$axios.$get('/api/', {
         params: {
           keyid: "cccd05138db6e13ac16bf8e63a21cd2d",
-          name: "肉"
+          name: this.keyword
         }
       }).then( res => {
         console.log('成功です。')
         console.log(res)
         this.shops = res.rest
+        this.$notify({
+          type: 'success',
+          title: '10店ヒットしました！',
+          message: `${this.keyword}の検索結果`,
+          position: 'bottom-left',
+          duration: 2000
+        })
       }).catch( err => {
         console.log('失敗です。')
         console.log(err)
@@ -43,7 +65,7 @@ export default {
 /* .container:before: -1; */
 /* header: 2; */
 
-.container {
+.main-container {
   position: relative;
   z-index: 1;
   margin: 0 auto;
@@ -59,7 +81,7 @@ export default {
   background-position: center; 
 }
 
-.container:before {
+.main-container:before {
   content: '';
   position: absolute;
   z-index: -1;
@@ -71,7 +93,22 @@ export default {
 }
 
 h2, h3, p {
-  margin-top: 30px;
+  margin: 30px 0;
+}
+
+.searched-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  background: #eee;
+}
+
+.shop-card {
+  flex-shrink: 0;
+  width: 400px;
+  height: 100px;
+  border: 1px solid gray;
+  margin: 10px 0;
 }
 
 </style>
