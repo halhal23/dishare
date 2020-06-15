30da75eca66f9eb121f784db55e607397fa6f390
Hiroharu Hara <hh.25799623@gmail.com>
2020-06-15 17:42:51 +0900

    [fix] 本番環境にて、Nuxtのタスクがヘルスチェックを通らないエラーを解消。

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   # API設計の観点からnamespaceを使用し、 /api/each_api 形式でrouting設定を行う
   namespace :api, format: 'json' do
     mount_devise_token_auth_for 'User', at: 'auth', controllers: {
