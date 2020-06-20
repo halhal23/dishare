778b4129859423c326bdeb7d24f9f82161e50777
Hiroharu Hara <hh.25799623@gmail.com>
2020-06-20 14:06:40 +0900

    [add] circleciで自動デプロイを構築。

--- backend/app/uploaders/image_uploader.rb
+++ backend/app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # developmentとtest以外はS3を使用
   if Rails.env.development?
-    storage :fog
+    storage :file
   elsif Rails.env.test?
     storage :file
   else
